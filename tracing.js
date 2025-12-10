a8672cff2fe9fd547f177d0a03ffa099cd8fe9e2
arkanadli <uzzomaki@gmail.com>
2025-12-10 15:00:23 +0700

    Update tracing.js

--- tracing.js
+++ tracing.js
@@ -1,12 +1,22 @@
+const fs = require('fs');
 const { NodeSDK } = require('@opentelemetry/sdk-node');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
 // const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
 const { BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
+const caCertPath = '/opt/otel-collector/collector.crt';
+
 
 const traceExporter = new OTLPTraceExporter({
-    url: 'https://10.17.17.42:4318/v1/traces',
+    // url: 'https://10.17.17.42:4318/v1/traces',
+    url: 'https://10.17.17.42:4318',
     // url: 'http://20.255.49.241:4317',
+
+    // Tambahkan ini untuk HTTPS + custom CA
+    httpsAgentOptions: {
+        ca: fs.readFileSync(caCertPath),
+        rejectUnauthorized: true, // wajib true kalau certificate valid
+    }
 });
 
 const sdk = new NodeSDK({
